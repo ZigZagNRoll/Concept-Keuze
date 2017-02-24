cae0fa3900a0f7d021c06d3f924ee5ce0d2cee81
Lukas Hanot <Lukas.Hanot@student.kdg.be>
2017-02-24 13:58:50 +0100

    Domain vraag toegevoegt

---
+++ Domain/Vraag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Vraag
+    {
+        public int VraagNummer { get; set; }
+        public string VraagTekst { get; set; }
+        public Thema Thema { get; set; }
+
+        public ICollection<Antwoord> Antwoorden { get; set; }
+    }
+}
